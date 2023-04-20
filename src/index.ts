36c2eaf303a3d251c567a210260accee8d99bd5f
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2023-04-19 22:48:14 -0300

    challenge 02 completed :rocket:

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerSetup from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSetup));
 
 app.use("/users", usersRoutes);
 
